c6deda086e19ea7671b9126b1b1724ce2cf590ef
Leo <leonidamvrosov@yandex.ru>
2023-10-11 18:58:51 +0500

    chat.01 completed

--- src/main/java/ru/gb/Main.java
+++ src/main/java/ru/gb/Main.java
@@ -1,7 +1,13 @@
 package ru.gb;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            new ConnectWindow(new LogHandler("./Chat_Log"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
